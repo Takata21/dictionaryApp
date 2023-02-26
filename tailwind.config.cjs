317fe994c124cc22b0ce4045dc2893f198f82d21
Manuel Takata <takata211011@gmail.com>
2023-02-26 00:10:52 -0500

    add personal font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Inter"],
+      serif: ["Noto Serif"],
+      mono: ["Roboto Mono"],
+    },
     extend: {},
   },
   plugins: [],
