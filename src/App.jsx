2008867fb9b74d5ab1e6687ccc4451fea69bdce6
Manuel Takata <takata211011@gmail.com>
2023-02-26 00:12:14 -0500

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Home from "./pages/Home/Home"
+
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Home />
     </div>
   )
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../../components/Navbar"
+
+function Home() {
+  return (
+    <div className="font-sans">
+      <Navbar />
+    </div>
+  )
+}
+
+export default Home
