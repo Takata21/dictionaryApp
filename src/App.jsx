4c1f4679ef534a5c586910b3469def7413049ac0
Manuel Takata <takata211011@gmail.com>
2023-03-02 17:26:04 -0500

    add theme and font from store

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import Home from './pages/Home/Home'
+import Home from './pages/Home'
+import { Route, Switch } from 'wouter'
+import Meaning from './pages/Meaning'
+import Navbar from './components/Navbar'
+import Search from './components/Search'
+import { useThemeStore } from './store/ThemeStore'
 
 function App() {
+  const { mode, font } = useThemeStore()
+  console.log(mode)
+  console.log(font)
   return (
-    <div className="App max-w-4xl m-auto w-full">
-      <Home />
+    <div className={`${mode === 'dark' ? 'dark' : ''} `}>
+      <div
+        className={
+          'App max-w-4xl m-auto w-full dark:bg-black dark:text-[#f5f5f5]'
+        }
+      >
+        <Navbar />
+        <Search />
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/meaning/:word" component={Meaning} />
+        </Switch>
+      </div>
     </div>
   )
 }
