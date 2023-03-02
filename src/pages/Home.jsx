ffddcc64060c27c6a4161fc36e378126b2a37e05
Manuel Takata <takata211011@gmail.com>
2023-03-02 17:25:35 -0500

    remove folder for home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Default from '../components/Default'
+
+function Home() {
+  return (
+    <main className="font-sans w-full m-auto p-5 overflow-hidden">
+      <Default />
+    </main>
+  )
+}
+
+export default Home
