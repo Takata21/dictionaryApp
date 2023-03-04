2b49d6be9b2317d7f1c27b150924bdb53d9f58c4
Manuel Takata <takata211011@gmail.com>
2023-03-03 22:44:35 -0500

    remove font class

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Default from '../components/Default'
 
 function Home() {
   return (
-    <main className="font-sans w-full m-auto p-5 overflow-hidden">
+    <main className="w-full m-auto p-5 overflow-hidden">
       <Default />
     </main>
   )
