4c190492b379cda2188471855e500b6cc5cf82f5
Manuel Takata <takata211011@gmail.com>
2023-03-07 22:06:34 -0500

    add loading and error property

--- src/store/WordStore.js
+++ src/store/WordStore.js
@@ -2,10 +2,16 @@ import { create } from 'zustand'
 const URL = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
 export const useWordStore = create((set) => ({
   meaning: [],
-  word: '',
+  loading: false,
+  error: false,
   getMeaning: async (payload) => {
-    set((state) => ({ ...state, word: payload }))
+    set((state) => ({ ...state, loading: true }))
     const meaning = await (await fetch(`${URL}${payload}`)).json()
+    meaning.title
+      ? set((state) => ({ ...state, error: true }))
+      : set((state) => ({ ...state, error: false }))
+
     set((state) => ({ ...state, meaning }))
+    set((state) => ({ ...state, loading: false }))
   },
 }))
